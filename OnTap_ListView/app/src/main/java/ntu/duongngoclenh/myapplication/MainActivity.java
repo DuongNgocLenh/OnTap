8f36fbf31dd6f25797c2ffed5d1c8ade3a512621
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-24 13:55:31 +0700

    tao listview va anh xa

--- OnTap_ListView/app/src/main/java/ntu/duongngoclenh/myapplication/MainActivity.java
+++ OnTap_ListView/app/src/main/java/ntu/duongngoclenh/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package ntu.duongngoclenh.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListView dthoai = (ListView) findViewById(R.id.DienThoai);
+        ArrayList<String> tendt = new ArrayList<String>();
+        tendt.add("SamSung");
+        tendt.add("Sony");
+        tendt.add("Iphone");
+        tendt.add("Xiaomi");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>();
+        
     }
 }
