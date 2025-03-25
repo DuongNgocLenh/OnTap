6a5aed34c0bf21802c7215fc48c571bfe6596f9a
DuongNgocLenh <lenh.dn.64cntt@ntu.edu.vn>
2025-03-25 18:32:06 +0700

    gan su kien lang nghe va xu ly sk tinh tong

--- LTGK_Bai1/app/src/main/java/ntu/duongngoclenh/ltgk_bai1/Cau1.java
+++ LTGK_Bai1/app/src/main/java/ntu/duongngoclenh/ltgk_bai1/Cau1.java
@@ -1,6 +1,9 @@
 package ntu.duongngoclenh.ltgk_bai1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,19 @@ public class Cau1 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        EditText soA = findViewById(R.id.edtA);
+        EditText soB = findViewById(R.id.edtB);
+        Button tinh = findViewById(R.id.btnTinh);
+        EditText ketqua = findViewById(R.id.edtKQ);
+        tinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(soA.getText().toString());
+                int b = Integer.parseInt(soB.getText().toString());
+                int tong = a + b;
+                ketqua.setText(String.valueOf(tong));
+            }
         });
     }
 }
--- LTGK_Bai1/app/src/main/java/ntu/duongngoclenh/ltgk_bai1/MainActivity.java
+++ LTGK_Bai1/app/src/main/java/ntu/duongngoclenh/ltgk_bai1/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.duongngoclenh.ltgk_bai1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,39 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button cau1,cau2,cau3,cau4;
+        cau1 = findViewById(R.id.btn1);
+        cau2 = findViewById(R.id.btn2);
+        cau3 = findViewById(R.id.btn3);
+        cau4 = findViewById(R.id.btn4);
+        cau1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mh1 = new Intent(MainActivity.this,Cau1.class);
+                startActivity(mh1);
+            }
+        });
+        cau2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mh2 = new Intent(MainActivity.this,Cau2.class);
+                startActivity(mh2);
+            }
+        });
+        cau3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mh3 = new Intent(MainActivity.this,Cau3.class);
+                startActivity(mh3);
+            }
+        });
+        cau4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mh4 = new Intent(MainActivity.this,Cau4.class);
+                startActivity(mh4);
+            }
         });
     }
 }
